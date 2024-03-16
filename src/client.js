9991256f42667edcefb83ecae5ebc328f99ee6e5
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-03-16 19:56:57 +0100

    add a clarification comment

--- src/client.js
+++ src/client.js
@@ -2,6 +2,8 @@ import copy from 'copy-to-clipboard';
 import logger from './logger.js';
 import { io } from "socket.io-client";
 
+// Set the thing below to ws://IP:42069
+// ^ where IP is the server's ip
 const socket = io('ws://localhost:42069');
 
 socket.on('sharedTo', (contents) => {
